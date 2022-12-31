5608fbce09215dce3dc5a2cd89d1405d2f7373fa
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2022-12-30 21:41:08 -0300

    feat(Continuação da aplicação - 01): Criando service de especificação e separando em modulos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/Cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/Cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
