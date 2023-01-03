5da6c5688e3fda02073ada061f400fd3a4c26646
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-01-02 22:10:45 -0300

    feat(Continuação da aplicação - 05): Refatorando a listagem de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,15 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../modules/Cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/Cars/UseCases/createCategory";
-
+import { listCategoriesController } from "../modules/Cars/UseCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
-
 
 categoriesRoutes.post("/", (req, res) => {
     return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
-    const all = categoriesRepository.list();
-    return res.status(200).json(all);
+    return listCategoriesController.handle(req, res);
 })
 
 export { categoriesRoutes }
