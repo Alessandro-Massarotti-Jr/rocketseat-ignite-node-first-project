1cc26b490ae4512d866f82a3109194f6e23cbeae
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-03-16 20:05:11 -0300

    feat(Iniciando a Api - 08): Criando use case de especificação

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,11 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/Cars/repositories/implementations/SpecifictionsRepository";
-import { CreateSpecificationService } from "../modules/Cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/Cars/UseCases/createSpecification";
 
 const specificationsRoutes = Router();
 
-const SpecificationsReposytory = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (req, res) => {
-
-    const { name, description } = req.body;
-
-    const createSpecificationService = new CreateSpecificationService(SpecificationsReposytory);
-    createSpecificationService.execute({ name, description });
-
-    return res.status(201).send();
-
+    return createSpecificationController.handle(req, res);
 })
 
 export { specificationsRoutes }
