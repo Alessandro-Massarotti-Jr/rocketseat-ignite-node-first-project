1191ce604d6af21d454ccd2255ee638dd53dd97e
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-03-19 14:58:46 -0300

    feat(Trabalhando com upload - 04): Conhecendo o conceito de stream

--- src/modules/Cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/Cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,18 @@
+import fs from "fs";
+import { parse, Parser } from "csv-parse";
+
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+
+        const stream = fs.createReadStream(file.path);
+        const parseFile = new Parser({ delimiter: ',' });
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
