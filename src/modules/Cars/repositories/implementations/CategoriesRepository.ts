cf900c8fd77d19a174a77a01a0ad6125274887ca
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-03-27 21:18:45 -0300

    feat(Trabalahndo com banco de dados - 07): Alterando o repositorio de categoria

--- src/modules/Cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/Cars/repositories/implementations/CategoriesRepository.ts
@@ -1,3 +1,4 @@
+import { getRepository, Repository } from "typeorm";
 import { Category } from "../../entities/Category";
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
 
@@ -5,12 +6,12 @@ import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesReposit
 
 class CategoriesRepository implements ICategoriesRepository {
 
-    private categories: Category[];
+    private repository: Repository<Category>;
 
     private static INSTANCE: CategoriesRepository;
 
     private constructor() {
-        this.categories = [];
+        this.repository = getRepository(Category);
     }
 
 
@@ -22,27 +23,31 @@ class CategoriesRepository implements ICategoriesRepository {
     }
 
 
-    create({ name, description }: ICreateCategoryDTO): void {
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
 
-        const category = new Category();
+        const category = this.repository.create(
+            {
+                description,
+                name
+            }
+        );
 
-        Object.assign(category, {
-            name,
-            description,
-            created_at: new Date()
+
+        await this.repository.save(category);
+        return;
+    }
+
+    async list(): Promise<Category[]> {
+        const categories = await this.repository.find();
+        return categories;
+    }
+
+    async findByName(name: string): Promise<Category> {
+        const category = await this.repository.findOne({
+            where: {
+                name: name
+            }
         });
-
-
-
-        this.categories.push(category);
-    }
-
-    list(): Category[] {
-        return this.categories
-    }
-
-    findByName(name: string): Category {
-        const category = this.categories.find(category => category.name === name);
         return category;
     }
 
