d8bb0e9bef264d0214249fc6a27784dde4b31b9c
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-05-07 10:12:30 -0300

    feat(Testes - 04): Teste de criação de categoria

--- src/modules/Cars/UseCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/Cars/UseCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,17 +1,42 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
 
-describe("Criar categoria", () => {
-    it("Espero que 2 + 2 seja 4", () => {
-        const soma = 2 + 2;
-        const resultado = 4;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+let createCategoryUseCase: CreateCategoryUseCase;
 
-        expect(soma).toBe(resultado);
-    })
 
-    it("Espero que 2 + 2 não seja 5", () => {
-        const soma = 2 + 2;
-        const resultado = 5;
+describe("Create Category", () => {
 
-        expect(soma).not.toBe(resultado);
-    })
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+    });
+
+    it("Should be able to create a new category", async () => {
+
+        const category = {
+            name: "Category Test",
+            description: "Category Test Description"
+        }
+        await createCategoryUseCase.execute(category);
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name);
+
+        expect(categoryCreated).toHaveProperty("id");
+
+    });
+
+    it("Should not be able to create a new category with name exists", async () => {
+        expect(async () => {
+            const category = {
+                name: "Category Test",
+                description: "Category Test Description"
+            }
+            await createCategoryUseCase.execute(category);
+            await createCategoryUseCase.execute(category);
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
 })
